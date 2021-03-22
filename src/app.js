063a28e18111cbbea3fea34711b1d052bebf9eae
Robert NIYITANGA <robertwilly668@gmail.com>
2021-03-22 04:26:57 +0200

    add passport,cloudinary,multer,

--- src/app.js
+++ src/app.js
@@ -2,24 +2,33 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+// import passport from 'passport';
+import configureGoogleStrategy from './middleware/passport.google';
 import userRoute from './routes/userRoute';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../upload/users`));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-Width,Content-Type,Accept,auth-token"
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-Width,Content-Type,Accept,Authorization'
   );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,PATCH,DELETE");
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,PATCH,DELETE');
     return res.status(200).json({});
   }
   next();
 });
+// app.use(passport.initialize());
+// app.use(passport.session());
+// // configureJWTStrategy();
+// configureGoogleStrategy.googleAuth();
+
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Welcome to the node backend API',
--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,7 +1,12 @@
-import cloudinary from 'cloudinary';
+import { config, uploader } from 'cloudinary';
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
+import dotenv from 'dotenv';
+dotenv.config();
+
+const cloudinaryConfig = () =>
+  config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+export { cloudinaryConfig, uploader };
